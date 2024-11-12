95dd316cc410b5b11ff97242b15c10dc546ad455
VasKaleev <vkaleev.fam@gmail.com>
2024-11-12 16:31:27 +0300

    basic app

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+import type { Metadata } from 'next'
+export const metadata: Metadata = {
+    title: 'About | Next App',
+  }
+export default function About() {
+    return <h3>Select subitem</h3>;
+}
---
+++ components/TheFooter.tsx
@@ -0,0 +1,8 @@
+const TheFooter = () => {
+    return (
+       <footer className="container">
+         Created by &copy;VasilyKaleev 
+       </footer> 
+    )
+}
+export {TheFooter};
