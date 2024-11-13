2e0bbb249169480057780245300b609ae5794bba
VasKaleev <vkaleev.fam@gmail.com>
2024-11-13 09:27:49 +0300

    fix error

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,12 +1,14 @@
 import { Metadata } from "next";
 
 async function getData(id: string) {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
-    next: {
-      revalidate: 60,
-    },
-  }
-)
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
   return response.json();
 }
 
@@ -17,19 +19,19 @@ type Props = {
 };
 
 export async function generateMetadata({
-  params: {id},
- }: Props):Promise<Metadata>  {
+  params: { id },
+}: Props): Promise<Metadata> {
   const post = await getData(id);
   return {
     title: post.title,
   };
 }
-export default async function Post({params: {id}}: Props) {
-    const post = await getData(id);
-    return (
-          <>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </>
-          );
+export default async function Post({ params: { id } }: Props) {
+  const post = await getData(id);
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
+  );
 }
