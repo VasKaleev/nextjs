a91e22a1e927ae2cccc0bd6cb130ef668ba3b300
VasKaleev <vkaleev.fam@gmail.com>
2024-11-16 14:35:46 +0300

    API

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,24 +1,3 @@
-// // 'use client';
-// import { getAllPosts } from '@/services/getPosts';
-// import {  Posts } from '@/components/Posts';
-// // import type { Metadata } from 'next'
-// // import { useEffect, useState } from 'react';
-// import { PostSearch } from '@/components/PostSearch';
-// import { usePosts } from '@/store';
-// import { shallow } from 'zustand/shallow';
-// // import { useEffect } from 'react';
-
-
-// export default  function Blog() {
-  
-//     return (
-//       <>
-//         <h1>Blog page</h1>
-//         <PostSearch />
-//         <Posts />
-//       </>
-//       );
-// }
 import { PostSearch } from "@/components/PostSearch";
 import { Posts } from "@/components/Posts";
 import { Metadata } from "next";
